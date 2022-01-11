3aa14d8cf7876c8b6d47e32829664b8db48eca0b
Gregório Neto <igregoriodoneto@outlook.com>
2022-01-10 23:40:22 -0300

    RPG2D - NPC e Iniciando a configuração do ScriptableObject

---
+++ Assets/Scripts/Dialog/DialogSettings.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Dialogue", menuName = "New Dialog/Dialogue")]
+public class DialogSettings : ScriptableObject
+{
+    [Header("Settings")]
+    public GameObject actor;
+
+    [Header("Dialogue")]
+    public Sprite speakerSprite;
+    public string sentence;
+
+    public List<Sentences> dialogues = new List<Sentences>();
+}
+
+[System.Serializable]
+public class Sentences
+{
+    public string actorName;
+    public Sprite profile;
+    public Languages sentence;
+}
+
+[System.Serializable]
+public class Languages
+{
+    public string portuguese;
+    public string english;
+    public string spanish;
+}
